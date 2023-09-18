8320b8d3ed6ad4f2e78d0dc75474b53634d536ae
Think Dev <=>
2023-09-18 14:58:43 +0100

    push my site to repo

---
+++ public_html/js/handleModal.js
@@ -0,0 +1,6 @@
+function pauseVideo(){
+// document.querySelector('video').pause();  ## it's Iframe not Video !
+// document.querySelector('iframe').setAttribute('src',''); ## delete source everytime.
+// document.querySelector('iframe').setAttribute('src',src);
+  window.location.reload();
+}
